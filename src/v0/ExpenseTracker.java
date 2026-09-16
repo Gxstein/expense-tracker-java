0e5e7ff0b07c0f273f91aedd9513df00100d9e28
Gxstein <mxrciodev@outlook.com>
2026-09-16 09:39:07 -0300

    Add menu options with switch

--- src/v0/ExpenseTracker.java
+++ src/v0/ExpenseTracker.java
@@ -11,6 +11,8 @@ public class ExpenseTracker {
         System.out.println("--Welcome to your Expense Tracker--");
 
         do {
+
+
             System.out.println("1 - Add expense");
             System.out.println("2 - Show total");
             System.out.println("3 - Show highest expense");
@@ -20,9 +22,25 @@ public class ExpenseTracker {
 
             select = sc.nextInt();
 
+            switch (select){
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+
+                default:
+                    System.out.println("(Enter a valid number between 0 and 4)");
+            }
+
         } while (select != 0);
 
-        System.out.println("The program ends here — see you next time!");
+        System.out.println("The program ends here - see you next time!");
 
         sc.close();
 
