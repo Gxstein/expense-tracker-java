c37c772599808800377729439b4ddc1c70238b95
Gxstein <mxrciodev@outlook.com>
2026-09-25 15:04:28 -0300

    Add Expense entity

--- src/v1/entities/Expense.java
+++ src/v1/entities/Expense.java
@@ -1,4 +1,65 @@
 package v1.entities;
 
+import v1.enums.Category;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Expense {
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+    private LocalDate date;
+    private Category category;
+
+    public Expense (){
+
+    }
+    public Expense(Long id, String description, BigDecimal amount, LocalDate date, Category category) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
