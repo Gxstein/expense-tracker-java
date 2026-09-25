8e5b6b4d42a66cc60656cf73745b03b956cc4466
Gxstein <mxrciodev@outlook.com>
2026-09-25 16:05:14 -0300

    Add add and remove methods to ExpenseManager

--- src/v1/entities/ExpenseManager.java
+++ src/v1/entities/ExpenseManager.java
@@ -1,4 +1,25 @@
 package v1.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpenseManager {
+
+    private List<Expense> expenses = new ArrayList<>();
+
+
+
+    public void add(Expense expense) {
+        expenses.add(expense);
+    }
+
+    public void remove(Long id){
+        for(Expense e : expenses){
+            if (e.getId().equals(id)) {
+                expenses.remove(e);
+                break;
+            }
+
+        }
+    }
 }
